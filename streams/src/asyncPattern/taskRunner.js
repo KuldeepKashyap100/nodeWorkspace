72de4f267fb1f822b086be3951a4e9767ebe7e0e
Kuldeep <kuldeep.kumar@cdk.com>
2021-08-29 18:59:56 +0530

    task runner

--- streams/src/asyncPattern/taskRunner.js
+++ streams/src/asyncPattern/taskRunner.js
@@ -40,3 +40,26 @@ const tasks = () => {
 
 const taskRunner = new TaskManager(tasks());
 taskRunner.run();
+
+// const putFilesAsync = (fileList, sftp, dirPath, dirName, __exports, activeFileWritingList, fileWriteCompletedList) => {
+//     return new Promise((resolve, reject) => {
+//       while(MAX_CONCURRET_WRITES > activeFileWritingList.length && fileList.length) {
+//         const fileName = fileList.shift();
+//         const currentTask = putFile(sftp, dirPath, dirName, fileName, __exports);
+//         activeFileWritingList.push(currentTask);
+//         currentTask.then(async () => {
+//           const currentTaskIdx = activeFileWritingList.findIndex(task => task === currentTask);
+//           fileWriteCompletedList.push(activeFileWritingList.splice(currentTaskIdx, 1));
+//           console.log(fileWriteCompletedList.length, activeFileWritingList.length);
+//           if(activeFileWritingList.length === 0) {
+//             return resolve(fileWriteCompletedList);
+//           }
+//           await putFilesAsync(fileList, sftp, dirPath, dirName, __exports, activeFileWritingList, fileWriteCompletedList, fileList);
+//           resolve(fileWriteCompletedList);
+//         })
+//         .catch((error) => {
+//           reject(error);
+//         });
+//       }
+//     });
+//   }
